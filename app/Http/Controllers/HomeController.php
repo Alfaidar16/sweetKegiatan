0ac4a4e7f5b1c4a4fcf28be0b6fd206b823addca
Alfaidar16 <alfaidaramirjr@gmail.com>
2024-06-19 15:13:55 +0800

    dd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use App\Charts\LaporanChart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; 
+
+
 class HomeController extends Controller
 {
-    public function Index(LaporanChart $laporanChart) {
+    public function Index(LaporanChart $LaporanChart) {
      
-      $data['laporanChart'] = $laporanChart->build();
+      $data['laporanChart'] = $LaporanChart->build();
         $with = [ 
             'title' => 'Dashboard',
-            'laporanChart' => $laporanChart->build()
+            'laporanChart' => $LaporanChart->build()
            
         ];
         return view('Auth.Dashboard')->with($with);
